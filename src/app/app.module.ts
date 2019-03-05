98ad2c854e27be784e0dc1477b37c84534700377
mammuthone <perconte@hotmail.it>
2019-03-05 09:01:49 +0100

    candidate release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { BodyComponent } from './body/body.component';
-import {  NgsRevealModule} from 'ngx-scrollreveal';
+import { NgsRevealModule } from 'ngx-scrollreveal';
+import { ElementComponent } from './components/element/element.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { SeparatorComponent } from './components/separator/separator.component';
+import { SpecsComponent } from './components/specs/specs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent
+    BodyComponent,
+    ElementComponent,
+    PortfolioComponent,
+    LanguagesComponent,
+    SeparatorComponent,
+    SpecsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     UiModule,
-    NgsRevealModule
+    NgsRevealModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+    }),
+    RoundProgressModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
-})
-export class BodyComponent implements OnInit {
+  styleUrls: ['./body.component.css'],
+  animations: [
+    trigger('changeState', [
+      state('state1', style({
+        backgroundColor: 'green',
+        transform: 'scale(1)',
+        opacity: 0.3
+      })),
+      state('state2', style({
+        backgroundColor: 'red',
+        transform: 'scale(1.5)',
+        opacity: 0.9
 
+      })),
+      transition('*=>state1', animate('1000ms cubic-bezier(.65,.08,.42,.92)')),
+      transition('*=>state2', animate('1000ms cubic-bezier(.65,.08,.42,.92)'))
+    ])
+  ]
+})
+export class BodyComponent implements OnInit, AfterViewInit {
+
+  toState = 'state1';
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+  }
+
+  changeState(stato: any) {
+    this.toState = stato;
   }
 
 }
